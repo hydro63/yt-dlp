c9f8f21926b91cbd2bc1b0bbe3f863b2b9a34761
locke <hamannsun@gmail.com>
2023-02-02 09:46:40 +0800

    custom changes

--- yt_dlp/__init__.py
+++ yt_dlp/__init__.py
@@ -984,6 +984,7 @@ def parse_options(argv=None):
         '_warnings': warnings,
         '_deprecation_warnings': deprecation_warnings,
         'compat_opts': opts.compat_opts,
+        'm3u8_fake_header': opts.m3u8_fake_header
     })
 
 
